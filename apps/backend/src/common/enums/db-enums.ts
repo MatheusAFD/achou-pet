687b95874c764882b2b3eb54686bcdb68c815e65
matheusafd <matheus.daf@outlook.com>
2025-05-08 18:00:00 -0300

    feat: add credential status enum to database enums

--- apps/backend/src/common/enums/db-enums.ts
+++ apps/backend/src/common/enums/db-enums.ts
@@ -3,6 +3,11 @@ import { pgEnum } from 'drizzle-orm/pg-core'
 import { z } from 'zod'
 
 export const pgRoleEnum = pgEnum('role', ['USER', 'ADMIN', 'SUPER_ADMIN'])
+export const pgCredentialsStatusEnum = pgEnum('credentialStatus', [
+  'ACTIVE',
+  'INACTIVE',
+  'PENDING'
+])
 export const pgAddressTypeEnum = pgEnum('address_type', [
   'PRIMARY',
   'SECONDARY'
@@ -10,3 +15,6 @@ export const pgAddressTypeEnum = pgEnum('address_type', [
 
 export const RoleEnum = z.enum(pgRoleEnum.enumValues).Enum
 export const AddressTypeEnum = z.enum(pgAddressTypeEnum.enumValues).Enum
+export const CredentialsStatusEnum = z.enum(
+  pgCredentialsStatusEnum.enumValues
+).Enum
