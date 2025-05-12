aff98790eed6e2b30fd44d1f0fb2519faa12d144
matheusafd <matheus.daf@outlook.com>
2025-05-12 19:47:21 -0300

    refactor(users): remove unique constraint from role field

--- apps/backend/src/drizzle/schema/users.ts
+++ apps/backend/src/drizzle/schema/users.ts
@@ -17,7 +17,7 @@ export const users = pgTable('users', {
   email: text('email').unique().notNull(),
   password: text('password').notNull(),
   phone: text('phone').notNull(),
-  role: pgRoleEnum().unique().notNull(),
+  role: pgRoleEnum().notNull(),
   canDisplayAddress: boolean('allow_show_address').notNull().default(false),
   lastLogin: timestamp('last_login'),
   ...timestamps
