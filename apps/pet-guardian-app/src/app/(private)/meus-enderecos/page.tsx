238eb0ddfe16a43b884895057c0ff47cd98f0a00
matheusafd <matheus.daf@outlook.com>
2025-05-14 20:53:34 -0300

    refactor(meus-enderecos): wrap button in CreateAddressModal

--- apps/pet-guardian-app/src/app/(private)/meus-enderecos/page.tsx
+++ apps/pet-guardian-app/src/app/(private)/meus-enderecos/page.tsx
@@ -1,14 +1,22 @@
+import { Metadata } from 'next'
+
 import { MapPinHouse } from 'lucide-react'
 
 import { Button, Container } from '@user-app/modules/@shared/components'
+import { CreateAddressModal } from '@user-app/modules/addresses/components'
+
+export const metadata: Metadata = {
+  title: 'Meus Endereços'
+}
 
 export default function MyAddressesPage() {
   return (
     <Container>
-      <Button className="w-fit ">
-        {' '}
-        <MapPinHouse /> Cadastrar novo
-      </Button>
+      <CreateAddressModal>
+        <Button className="w-fit">
+          <MapPinHouse /> Cadastrar novo
+        </Button>
+      </CreateAddressModal>
     </Container>
   )
 }
