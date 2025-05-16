965eabce991150f5084256691bf29c20ae8637f9
matheusafd <matheus.daf@outlook.com>
2025-05-16 14:21:03 -0300

    feat(credentials): add primary address and pet retrieval

--- apps/backend/src/modules/credentials/use-cases/get-credential-details.use-case.ts
+++ apps/backend/src/modules/credentials/use-cases/get-credential-details.use-case.ts
@@ -1,6 +1,6 @@
 import { Inject, NotFoundException } from '@nestjs/common'
 
-import { eq } from 'drizzle-orm'
+import { eq, and } from 'drizzle-orm'
 
 import { plainToClass } from 'class-transformer'
 import { DrizzleSchema } from 'src/drizzle/types'
@@ -8,7 +8,9 @@ import { DrizzleSchema } from 'src/drizzle/types'
 import { DrizzleAsyncProvider } from '@db/drizzle/drizzle.provider'
 import { addresses, users } from '@db/drizzle/schema'
 import { credentials } from '@db/drizzle/schema/credentials'
+import { pets } from '@db/drizzle/schema/pets'
 
+import { Address } from '@modules/addresses/entities/address.entity'
 import { User } from '@modules/users/entities/user.entity'
 
 export class GetCredentialDetailsUseCase {
@@ -23,9 +25,7 @@ export class GetCredentialDetailsUseCase {
         credential: {
           id: credentials.id,
           status: credentials.status,
-          batchId: credentials.batchId,
-          userId: credentials.userId,
-          activatedAt: credentials.activatedAt
+          userId: credentials.userId
         },
         user: {
           name: users.name,
@@ -33,23 +33,46 @@ export class GetCredentialDetailsUseCase {
           email: users.email,
           phone: users.phone,
           canDisplayAddress: users.canDisplayAddress
-        },
-        addresses
+        }
       })
       .from(credentials)
       .where(eq(credentials.id, id))
       .leftJoin(users, eq(users.id, credentials.userId))
-      .leftJoin(addresses, eq(addresses.userId, users.id))
       .limit(1)
 
-    if (!result || !result.credential) {
+    if (!result.credential) {
       throw new NotFoundException('Credential not found')
     }
+
+    let primaryAddress: Address | null = null
+
+    console.log('result', result.user)
+
+    if (!!result.user?.canDisplayAddress && result.credential.userId) {
+      const [address] = await this.db
+        .select()
+        .from(addresses)
+        .where(
+          and(
+            eq(addresses.userId, result.credential.userId),
+            eq(addresses.type, 'PRIMARY')
+          )
+        )
+        .limit(1)
+      primaryAddress = address ? (address as Address) : null
+    }
+
+    const [pet] = await this.db
+      .select()
+      .from(pets)
+      .where(eq(pets.credentialId, id))
+      .limit(1)
 
     return {
       ...result.credential,
       user: plainToClass(User, result.user),
-      address: result.addresses
+      primaryAddress,
+      pet
     }
   }
 }
