6f94403bd030943a7fe76f218e3ac71bbb186b0d
matheusafd <matheus.daf@outlook.com>
2025-05-17 22:44:17 -0300

    fix(dto): change photo type to File in UpdatePetDto

--- apps/backend/src/modules/pets/dto/update-pet.dto.ts
+++ apps/backend/src/modules/pets/dto/update-pet.dto.ts
@@ -4,5 +4,5 @@ import { CreatePetDto } from './create-pet.dto'
 
 export class UpdatePetDto extends PartialType(CreatePetDto) {
   @ApiProperty({ required: false, type: 'string', format: 'binary' })
-  photo?: any
+  photo?: File
 }
