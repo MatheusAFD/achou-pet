84f01ff74924ac8b38cbc88738e9424307ede973
matheusafd <matheus.daf@outlook.com>
2025-05-18 13:22:24 -0300

    feat(image-field): add delete functionality for uploaded images

--- apps/pet-guardian-app/src/modules/@shared/components/fields/image-field/index.tsx
+++ apps/pet-guardian-app/src/modules/@shared/components/fields/image-field/index.tsx
@@ -2,9 +2,13 @@ import { useRef } from 'react'
 import type { ChangeEvent } from 'react'
 import { useController, FieldValues, Control, Path } from 'react-hook-form'
 
-import Image from 'next/image'
+import { Trash2 } from 'lucide-react'
 
+import { Button } from '@user-app/modules/@shared/components/ui'
 import { cn } from '@user-app/modules/@shared/utils'
+
+import { ChildTooltip } from '../../child-tooltip'
+import { Avatar, AvatarImage } from '../../ui'
 
 export interface ImageFieldProps<T extends FieldValues> {
   name: Path<T>
@@ -36,32 +40,74 @@ export function ImageField<T extends FieldValues>({
     if (file.size > maxSizeMB * 1024 * 1024) {
       return
     }
+
     onChange(file)
   }
+
+  function handleDelete() {
+    onChange(undefined)
+    if (inputRef.current) inputRef.current.value = ''
+  }
+
+  function isFile(v: unknown): v is File {
+    return v instanceof File
+  }
+
+  function getImageSrc(): string | null {
+    if (isFile(value)) {
+      return URL.createObjectURL(value)
+    }
+
+    if (typeof value === 'string' && value) {
+      return value
+    }
+    return null
+  }
+
+  const imageSrc = getImageSrc()
 
   return (
     <div className="flex flex-col items-center gap-2">
       <div
         className={cn(
-          'w-24 h-24 rounded-full bg-gray-100 flex items-center justify-center cursor-pointer border border-dashed border-gray-300',
+          'size-32 rounded-full bg-gray-100 flex items-center justify-center cursor-pointer border border-dashed border-gray-300 relative',
           error && 'border-red-500'
         )}
         onClick={() => inputRef.current?.click()}
       >
-        {value ? (
-          <Image
-            src={typeof value === 'string' ? value : URL.createObjectURL(value)}
-            width={96}
-            height={96}
-            alt="Avatar preview"
-            className="w-full h-full object-cover rounded-full"
-          />
+        {imageSrc ? (
+          <>
+            <Avatar className="w-full h-full outline-2 outline-primary">
+              <AvatarImage
+                src={imageSrc}
+                width={128}
+                height={128}
+                alt="Avatar preview"
+                className="w-full h-full object-cover rounded-full"
+              />
+            </Avatar>
+            <ChildTooltip content="Remover foto">
+              <Button
+                type="button"
+                size="icon"
+                className="absolute  -bottom-4 left-1/2 -translate-x-1/2 z-10 rounded-full"
+                variant="destructive"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  handleDelete()
+                }}
+              >
+                <Trash2 className="w-4 h-4" />
+              </Button>
+            </ChildTooltip>
+          </>
         ) : (
           <span className="text-gray-400 text-xs text-center">
             {label || 'Inserir foto'}
           </span>
         )}
       </div>
+
       <input
         ref={inputRef}
         type="file"
