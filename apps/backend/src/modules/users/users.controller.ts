0fd22da606372e4c5f40273c37393a6240d27c0f
matheusafd <matheus.daf@outlook.com>
2025-05-19 14:07:19 -0300

    refactor(users): simplify UsersController constructor

--- apps/backend/src/modules/users/users.controller.ts
+++ apps/backend/src/modules/users/users.controller.ts
@@ -3,15 +3,11 @@ import { Controller, Post, Body } from '@nestjs/common'
 import { Public } from '@common/decorators/auth'
 
 import { CreateUserDto } from './dto/create-user.dto'
-import { GetUserCredentialsUseCase } from './use-cases'
 import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly getUserCredentialsUseCase: GetUserCredentialsUseCase
-  ) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Post()
   @Public()
