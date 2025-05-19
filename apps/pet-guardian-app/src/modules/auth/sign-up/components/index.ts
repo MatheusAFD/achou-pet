18e83197a55e99d5a6db16354ad1d78dd8824b9e
matheusafd <matheus.daf@outlook.com>
2025-05-19 20:34:33 -0300

    feat(sign-up): export user-related components for registration

--- apps/pet-guardian-app/src/modules/auth/sign-up/components/index.ts
+++ apps/pet-guardian-app/src/modules/auth/sign-up/components/index.ts
@@ -1,1 +1,4 @@
-export * from './register-user-form'
+export * from './user-data-step'
+export * from './register-user'
+export * from './user-password-step'
+export * from './user-email-confirmation-step'
