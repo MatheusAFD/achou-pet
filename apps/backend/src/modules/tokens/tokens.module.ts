cec7909df0d6701913caf938697524388b9acf71
matheusafd <matheus.daf@outlook.com>
2025-05-19 22:29:41 -0300

    refactor(tokens): update module imports for clarity

--- apps/backend/src/modules/tokens/tokens.module.ts
+++ apps/backend/src/modules/tokens/tokens.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
+
+import { DrizzleModule } from '@modules/drizzle/drizzle.module'
+import { EmailsModule } from '@modules/emails/emails.module'
 
 import { TokensController } from './tokens.controller'
 import { TokensService } from './tokens.service'
 
 @Module({
+  imports: [DrizzleModule, forwardRef(() => EmailsModule)],
   controllers: [TokensController],
   providers: [TokensService],
   exports: [TokensService]
