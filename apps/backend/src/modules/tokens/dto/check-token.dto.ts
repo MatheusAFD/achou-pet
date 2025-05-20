045eb276e5268bb586d8570ea0b66ef42d3bb3c7
matheusafd <matheus.daf@outlook.com>
2025-05-19 22:29:54 -0300

    refactor(tokens): add validation decorators to DTOs

--- apps/backend/src/modules/tokens/dto/check-token.dto.ts
+++ apps/backend/src/modules/tokens/dto/check-token.dto.ts
@@ -1,4 +1,10 @@
+import { IsString, MinLength } from 'class-validator'
+
 export class CheckTokenDto {
+  @IsString()
+  @MinLength(6)
   value: string
+
+  @IsString()
   key: string
 }
--- apps/backend/src/modules/tokens/dto/create-token.dto.ts
+++ apps/backend/src/modules/tokens/dto/create-token.dto.ts
@@ -1,3 +1,6 @@
+import { IsString } from 'class-validator'
+
 export class CreateTokenDto {
+  @IsString()
   key: string
 }
