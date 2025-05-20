8e3ea314fb6e27666306f27698ba98c228369b50
matheusafd <matheus.daf@outlook.com>
2025-05-19 22:29:37 -0300

    refactor(tokens): change check-token route to POST

--- apps/backend/src/modules/tokens/tokens.controller.ts
+++ apps/backend/src/modules/tokens/tokens.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body } from '@nestjs/common'
+
+import { Public } from '@common/decorators/auth'
 
 import { CheckTokenDto } from './dto/check-token.dto'
 import { CreateTokenDto } from './dto/create-token.dto'
@@ -8,12 +10,14 @@ import { TokensService } from './tokens.service'
 export class TokensController {
   constructor(private readonly tokensService: TokensService) {}
 
+  @Public()
   @Post()
   create(@Body() createTokenDto: CreateTokenDto) {
     return this.tokensService.sendToken(createTokenDto)
   }
 
-  @Get('check-token')
+  @Public()
+  @Post('check-token')
   findOne(@Body() body: CheckTokenDto) {
     return this.tokensService.check(body)
   }
