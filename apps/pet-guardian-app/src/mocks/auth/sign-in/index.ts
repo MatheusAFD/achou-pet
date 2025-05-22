bdad51606f8c193085e92ccc890a5e6cda1cffc0
matheusafd <matheus.daf@outlook.com>
2025-05-21 21:28:29 -0300

    feat(mocks): add sign-in mock and JWT token generator

---
+++ apps/pet-guardian-app/src/mocks/auth/sign-in/index.ts
@@ -0,0 +1,22 @@
+import { http, HttpResponse } from 'msw'
+
+import { generateMockJwtToken } from '@user-app/modules/mocks/utils'
+
+export const signInMock = [
+  http.post<never, { email: string; password: string }>(
+    'http://localhost:4000/api/auth/sign-in',
+    async ({ request }) => {
+      const { email, password } = await request.json()
+
+      if (email === 'johndoe@example.com' && password === 'password') {
+        const accessToken = generateMockJwtToken()
+        const refreshToken = 'sample-refresh-token'
+
+        return new HttpResponse(JSON.stringify({ accessToken, refreshToken }), {
+          status: 201
+        })
+      }
+      return new HttpResponse({}, { status: 401 })
+    }
+  )
+]
