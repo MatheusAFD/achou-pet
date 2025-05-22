c5c07c9f09477f82af18c0be2e63772e6ef96b53
matheusafd <matheus.daf@outlook.com>
2025-05-22 19:29:56 -0300

    chore: update next.config.ts for standalone output

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.r2.cloudflarestorage.com'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
