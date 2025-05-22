df5c76636a9b81ef66b2e6a9437afe9ed249df4b
matheusafd <matheus.daf@outlook.com>
2025-05-22 11:13:52 -0300

    feat(next.config): add rewrites for auth sign-in route

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
-export default nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/auth-sign-in'
+      }
+    ]
+  }
+}
+
+export default nextConfig
