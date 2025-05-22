8b77a421c38e2b7e870e2cc83cbc75cd0d3d61c5
matheusafd <matheus.daf@outlook.com>
2025-05-22 20:52:34 -0300

    chore: update migration path to use process.cwd()

--- apps/backend/src/drizzle/migrate.ts
+++ apps/backend/src/drizzle/migrate.ts
@@ -19,7 +19,8 @@ void (async () => {
     }
   })
 
-  const migrationPath = path.join(__dirname, 'migrations')
+  const migrationPath = path.join(process.cwd(), 'src/drizzle/migrations')
+
   await migrate(db, { migrationsFolder: migrationPath })
 
   console.log('Migration complete')
