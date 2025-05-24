47408146b36d6b30f1fd2f8311140a1d2a443038
matheusafd <matheus.daf@outlook.com>
2025-05-24 16:29:18 -0300

    refactor(app-sidebar): update link to admin credentials

--- apps/admin/src/modules/@shared/components/app-sidebar/index.tsx
+++ apps/admin/src/modules/@shared/components/app-sidebar/index.tsx
@@ -20,7 +20,7 @@ export const AppSidebar = () => {
   return (
     <Sidebar>
       <SidebarHeader className="flex flex-row items-center gap-4 h-[72px] border-b border-sidebar-border">
-        <Link href="/meus-pets">
+        <Link href="/admin/credenciais">
           <Image
             src="/logo.png"
             width={64}
