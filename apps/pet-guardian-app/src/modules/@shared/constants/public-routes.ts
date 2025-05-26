f3e6b935ecb437dd13bb7021ddce2c381eaf897e
matheusafd <matheus.daf@outlook.com>
2025-05-26 15:54:10 -0300

    feat: add 'guia-de-uso' route to public routes

--- apps/pet-guardian-app/src/modules/@shared/constants/public-routes.ts
+++ apps/pet-guardian-app/src/modules/@shared/constants/public-routes.ts
@@ -2,5 +2,6 @@ export const publicRoutes = [
   { path: '/auth/sign-in', whenAuthenticated: 'redirect' },
   { path: '/auth/sign-up', whenAuthenticated: 'redirect' },
   { path: '/auth/sign-out', whenAuthenticated: 'keep' },
+  { path: '/guia-de-uso', whenAuthenticated: 'keep' },
   { path: '/pet', whenAuthenticated: 'keep' }
 ] as const
