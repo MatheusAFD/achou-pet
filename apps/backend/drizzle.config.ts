70082a0e3051b52394c645f5a4fb063c396b46ed
matheusafd <matheus.daf@outlook.com>
2025-05-28 17:38:36 -0300

    refactor(drizzle.config.ts): simplify schema and migrations paths

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,11 +1,9 @@
 import { Config, defineConfig } from 'drizzle-kit'
 
-const isProd = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
   dialect: 'postgresql',
-  schema: isProd ? './dist/src/drizzle/schema' : './src/drizzle/schema',
-  out: isProd ? './dist/src/drizzle/migrations' : './src/drizzle/migrations',
+  schema: './dist/src/drizzle/schema',
+  out: './dist/src/drizzle/migrations',
   dbCredentials: {
     url:
       process.env.DATABASE_URL ??
