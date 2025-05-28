2b2c170b49598b7ce46d4515ef8ee93cffe0625d
matheusafd <matheus.daf@outlook.com>
2025-05-28 15:21:43 -0300

    refactor(drizzle.config.ts): simplify schema path resolution

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,15 +1,9 @@
 import { Config, defineConfig } from 'drizzle-kit'
 
-import fs from 'fs'
-
-function resolvePath(primary: string, fallback: string) {
-  return fs.existsSync(primary) ? primary : fallback
-}
-
 export default defineConfig({
   dialect: 'postgresql',
-  schema: resolvePath('./dist/src/drizzle/schema', './src/drizzle/schema'),
-  out: resolvePath('./dist/src/drizzle/migrations', './src/drizzle/migrations'),
+  schema: './src/drizzle/schema',
+  out: './src/drizzle/migrations',
   dbCredentials: {
     url:
       process.env.DATABASE_URL ??
