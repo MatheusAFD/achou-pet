7609e8fc6fcec9382eaa26c2729d9d5f47418719
matheusafd <matheus.daf@outlook.com>
2025-05-28 18:16:58 -0300

    refactor(drizzle.config.ts): specify schema file path

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { Config, defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/drizzle/schema',
+  schema: './src/drizzle/schema/index.ts',
   out: './src/drizzle/migrations',
   dbCredentials: {
     url:
