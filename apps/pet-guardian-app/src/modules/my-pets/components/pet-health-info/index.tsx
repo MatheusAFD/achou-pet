008a681d2a1621764e8ef5808a5b613d13fd30f5
matheusafd <matheus.daf@outlook.com>
2025-07-26 14:33:53 -0300

    feat: add PetHealthInfo component for displaying health data

---
+++ apps/pet-guardian-app/src/modules/my-pets/components/pet-health-info/index.tsx
@@ -0,0 +1,44 @@
+import { BeanOff, Pill, Syringe } from 'lucide-react'
+
+import { ChildTooltip } from '@user-app/modules/@shared/components'
+
+interface PetHealthInfoProps {
+  healthData: {
+    isVaccinated: boolean
+    needsMedication: boolean
+    hasAllergies: boolean
+  }
+}
+
+export const PetHealthInfo = ({ healthData }: PetHealthInfoProps) => {
+  const { isVaccinated, needsMedication, hasAllergies } = healthData
+
+  return (
+    <div className="absolute bottom-3 right-3.5 flex gap-2">
+      <ChildTooltip content={isVaccinated ? 'Vacinado' : 'Não vacinado'}>
+        <Syringe
+          size={16}
+          className={isVaccinated ? 'text-tertiary' : 'text-gray-300'}
+        />
+      </ChildTooltip>
+
+      <ChildTooltip
+        content={
+          needsMedication ? 'Necessita remédio' : 'Não necessita remédio'
+        }
+      >
+        <Pill
+          size={16}
+          className={needsMedication ? 'text-tertiary' : 'text-gray-300'}
+        />
+      </ChildTooltip>
+
+      <ChildTooltip content={hasAllergies ? 'Alérgico' : 'Sem alergias'}>
+        <BeanOff
+          size={16}
+          className={hasAllergies ? 'text-tertiary' : 'text-gray-300'}
+        />
+      </ChildTooltip>
+    </div>
+  )
+}
