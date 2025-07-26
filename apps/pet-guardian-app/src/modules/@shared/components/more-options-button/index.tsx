aaa553cd5c4da576d07e64c1bf3f59b749fd264f
matheusafd <matheus.daf@outlook.com>
2025-07-26 14:33:44 -0300

    feat: add MoreOptionsMenu component

---
+++ apps/pet-guardian-app/src/modules/@shared/components/more-options-button/index.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren } from 'react'
+
+import { Ellipsis } from 'lucide-react'
+
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuTrigger
+} from '../ui'
+
+export const MoreOptionsMenu = (props: PropsWithChildren) => {
+  const { children } = props
+
+  return (
+    <DropdownMenu
+      onOpenChange={() => {
+        setTimeout(() => (document.body.style.pointerEvents = ''), 100)
+      }}
+    >
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Ellipsis className="size-5 text-tertiary" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuGroup>{children}</DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
